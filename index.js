34fc4b1fcdfc87a76156b7d47301a1ac71460d9d
Matt Krueger <mkruege2@gmail.com>
2015-07-29 09:23:21 -0500

    Add password generation pass -g label secret

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ var path = require('path');
 var words = require('./words');
 var filePath = path.resolve('.', 'words.json');
 var _ = require('lodash-node/underscore');
+var generatePassword = require('password-generator');
 
-var encrypt = function encrypt(label, message, secret){
+var encrypt = function encrypt(){
+    var label, message, secret;
+    if (arguments[0]==="-g"){
+        label = arguments[1];
+        secret = arguments[2];
+        message = generatePassword(18, false);
+        console.log(message);
+    }
+
     var encrypted = crypto.Rabbit.encrypt(message, secret);
     words[label] = "" + encrypted;
     var newWords = JSON.stringify(words, null, 2);
@@ -21,16 +30,17 @@ var decrypt = function decrypt(label, secret){
     console.log("%s", decrypted.toString(crypto.enc.Utf8));
     //var exec = require('child_process').exec;
     //exec("echo " + decrypted.toString(crypto.enc.Utf8) + " | pbcopy", function(){console.log('done')});
+    //exec("history -c");
 };
 
 var len = process.argv.length;
 var ENCRYPT_ARGS_LEN = 5;
 var DECRYPT_ARGS_LEN = 4;
-
+var args;
 if (len === ENCRYPT_ARGS_LEN){
-    var args = [].concat(process.argv).splice(2-ENCRYPT_ARGS_LEN);
+    args = [].concat(process.argv).splice(2-ENCRYPT_ARGS_LEN);
     encrypt.apply(this, args);
 } else if (len === DECRYPT_ARGS_LEN){
-    var args = [].concat(process.argv).splice(2-DECRYPT_ARGS_LEN);
+    args = [].concat(process.argv).splice(2-DECRYPT_ARGS_LEN);
     decrypt.apply(this, args);
 }
